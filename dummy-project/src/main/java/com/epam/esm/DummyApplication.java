bdfe5d2fccd214fcae986e58b0fb10af5c2f86c2
Roman <roma.bruhanchik@gmail.com>
2021-08-13 09:29:12 +0300

    some fixes

---
+++ dummy-project/src/main/java/com/epam/esm/DummyApplication.java
@@ -0,0 +1,11 @@
+package com.epam.esm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DummyApplication.class, args);
+    }
+}
